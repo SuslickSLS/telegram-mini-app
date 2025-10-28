7cfaa296407bea6f6e1d129cb9ca34eee3e26a7f
unknown <Admin@BOOK036.corp.local>
2025-10-28 12:51:39 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Telegram Mini App</h1>
+      <p>React приложение успешно создано!</p>
+    </div>
+  )
+}
+
+export default App  
