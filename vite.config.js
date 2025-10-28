ed86a2c9c703c7c7da8017eb6919f082aefeb91d
unknown <Admin@BOOK036.corp.local>
2025-10-28 12:58:42 +0500

    Add GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // или '/telegram-mini-app/' если хотите в подпапке
+  base: './', 
   build: {
     outDir: 'dist',
     sourcemap: false,
